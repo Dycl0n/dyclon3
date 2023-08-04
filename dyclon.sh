e38430e6328ad73264c1f2ed54d3ddc59a609af0
Dustin Clemetson <dycl0n@Dycl0ns-Mac.local>
2023-08-04 01:49:21 -0700

    new

---
+++ dyclon.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+function display_ascii_art2() {
+    cat <<-EOF 
+░█▀▄░█░█░█▀▀░█░░░█▀█░█▀█░░░▀█▀░█▀█░█▀▀░▀█▀░█▀█░█░░░█░░░█▀▀░█▀▄
+░█░█░░█░░█░░░█░░░█░█░█░█░░░░█░░█░█░▀▀█░░█░░█▀█░█░░░█░░░█▀▀░█▀▄
+░▀▀░░░▀░░▀▀▀░▀▀▀░▀▀▀░▀░▀░░░▀▀▀░▀░▀░▀▀▀░░▀░░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀
+EOF
+}
+
+display_ascii_art2 
+
+folder_path="$HOME/dycl0n"  # Replace with the actual folder path
+
+echo " Dyclon Installer"
+read -p "Do you want to copy the contents of $folder_path to your home folder? (yes/no): " answer
+
+if [ "$answer" = "yes" ]; then
+  # Copy all contents within the folder to the home folder
+  cp -R "$folder_path"/.[!.]* "$HOME/"
+  cp -R "$folder_path"/* "$HOME/"
+  echo "All contents copied to the home folder."
+elif [ "$answer" = "no" ]; then
+  echo "No action taken. Contents not copied."
+else
+  echo "Invalid input. Please enter 'yes' or 'no'."
+fi
